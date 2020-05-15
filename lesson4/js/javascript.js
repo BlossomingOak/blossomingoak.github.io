ce9e7395fa1b83f477e22c4c850b0332008b7289
BlossomingOak <blossomingoakmedia@gmail.com>
2020-05-15 08:04:23 -0600

    update to assignment portal and to lesson 4 (almost done with lesson 4, just need help with the menu not showing up properly)

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,3 +1,3 @@
-function toggleMenu(){
-    Document.getElementByClassName("navigation")[0].classList.toggle("responsive");
+function toggleMenu() {
+    document.getElementById("nav").classList.toggle("open");
 }
