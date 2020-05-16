888ec16a0a485810090c79828c51256afa9a4994
BlossomingOak <blossomingoakmedia@gmail.com>
2020-05-16 11:32:24 -0600

    trying to update javascript date function lesson 4

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
     document.getElementById("nav").classList.toggle("open");
 }
+function getDate() {
+let days = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
+let months = [January, February, March, April, May, June, July, August, September, October, November, December];
+let today = new Date();
+let thisDay = days[today.getDay()];
+let thisMonth = months[today.getMonth()];
+let date = thisDay + ", " + today.getDate() + " " + thisMonth + " " + today.getFullYear();
+document.getelementById("date").innerHTML = date; 
+}
+
+
